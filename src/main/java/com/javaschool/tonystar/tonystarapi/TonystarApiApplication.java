1b1dcaa838806e559319148e8a719354d7f92413
Anton Isakov <electroautomatic@gmail.com>
2023-10-26 15:00:10 +0200

    correct domain diagram, spring start

---
+++ src/main/java/com/javaschool/tonystar/tonystarapi/TonystarApiApplication.java
@@ -0,0 +1,13 @@
+package com.javaschool.tonystar.tonystarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonystarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonystarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaschool/tonystar/tonystarapi/TonystarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaschool.tonystar.tonystarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonystarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
